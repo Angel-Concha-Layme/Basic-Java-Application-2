5305c76f1d65afdd99bfa6d35ab1ef441fa86017
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-02-27 16:04:37 -0500

    Update Index.java

--- Index.java
+++ Index.java
@@ -1,44 +1,71 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
 
 public class Index extends JFrame implements ActionListener{
-    private JLabel lblnombre,lblIcon;
-    private JTextField txtnombre;
-    private JButton btnIngresar;
+    private JLabel lblIcon, lblCorreo, lblBienvenido, lblContrasenia;
+    private JTextField txtCorreo, txtContrasenia;
+    private JButton btnIniciar;
+    private String correo;
 
 
     public Index(){
         setLayout(null);
-        setTitle("Bienvenido");
+        setTitle("Inicio");
+        getContentPane().setBackground(new Color(0,153,255));
 
-        lblIcon = new JLabel();
-    
-        lblnombre = new JLabel("Nombre");
-        lblnombre.setBounds(50,50,100,30);
-        add(lblnombre);
+        ImageIcon icon = new ImageIcon(getClass().getResource("Images/login.png"));
+        lblIcon = new JLabel(icon);
+        lblIcon.setBounds(70,0,icon.getIconWidth(),icon.getIconHeight());                  
+        add(lblIcon);   
+        
+        lblBienvenido = new JLabel("Bienvenido");
+        lblBienvenido.setBounds(80,190,300,40);
+        lblBienvenido.setFont(new Font("Arial",Font.BOLD,35));
+        lblBienvenido.setForeground(Color.black);
+        add(lblBienvenido);
 
-        txtnombre = new JTextField();
-        txtnombre.setBounds(150,50,150,30);
-        add(txtnombre);
+        
+        lblCorreo = new JLabel("Correo");
+        lblCorreo.setBounds(70,250,150,20);
+        lblCorreo.setForeground(Color.black);
+        lblCorreo.setFont(new Font("Arial",Font.BOLD,14));
+        add(lblCorreo);
 
-        btnIngresar = new JButton("Ingresar");
-        btnIngresar.setBounds(150,100,100,30);
-        btnIngresar.addActionListener(this);
-        add(btnIngresar);
+        txtCorreo = new JTextField();
+        txtCorreo.setBounds(70,270,200,25);
+        txtCorreo.setForeground(Color.black);
+        txtCorreo.setFont(new Font("Arial",0,14));
+        add(txtCorreo);
+
+
+        btnIniciar = new JButton("Iniciar");
+        btnIniciar.setBounds(70,310,100,25);
+        btnIniciar.setBackground(new Color(255,255,255)); 
+        btnIniciar.setForeground(Color.black);
+        btnIniciar.setFont(new Font("Arial",Font.BOLD,14));
+        btnIniciar.addActionListener(this);
+        add(btnIniciar);
     }
 
     public void actionPerformed(ActionEvent e){
-        if(e.getSource()==btnIngresar){
-            String nombre = txtnombre.getText();
-            if(nombre.equals("")){
-                JOptionPane.showMessageDialog(null, "Ingrese su nombre");
+        if(e.getSource()==btnIniciar){
+            correo = txtCorreo.getText();
+            if(correo.equals("")){
+                JOptionPane.showMessageDialog(null,"Ingrese su correo");
+                return;
+            }
+            if(correo.matches("[a-zA-Z0-9]{1,}@[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z0-9]{1,}")==false){
+                JOptionPane.showMessageDialog(null,"Ingrese un correo valido");
+            }
+            else{
             }
         }
     }
 
     public static void main(String args[]){   
         Index index = new Index();
-        index.setBounds(0,0,400,400);
+        index.setBounds(0,0,350,450);
         index.setVisible(true);
         index.setResizable(false);
         index.setLocationRelativeTo(null);
